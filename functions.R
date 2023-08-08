fe38562dcef1cd703aa64d4cd506c378a80fd431
Ivy K <98768550+ivykos@users.noreply.github.com>
2023-08-08 12:30:20 -0400

    Add files via upload

--- functions.R
+++ functions.R
@@ -3,30 +3,26 @@ library(Matrix)
 library(tidyverse)
 
 
-
-#Create pseudo-bulk counts file
+#Define pseudobulk function
 pseudobulk <- function(obj){
   matrix <- GetAssayData(obj, slot="counts")
   df <- data.frame(rownames(matrix))
-  samples <- as.character(unique(obj@meta.data$projid))
-  for (i in samples){
-    matrix <- GetAssayData(subset(obj, subset = projid == i), slot="counts")
+  for (i in unique(obj@meta.data$projid_region)){
+    matrix <- GetAssayData(subset(obj, subset = projid_region == as.character(i)))
     bulk <- data.frame(Matrix::rowSums(matrix))
     df[as.character(i)] <- bulk[1]
     
   }
   df <- data.frame(df, row.names = 1)
-  names(df) <- sub("^X","",names(df)) 
   return(df)
-  write.csv(df, "bulk.csv")
 }
 
-#Generate Deseq ColData
+#Define function to make DESeq design matrix
 coldata <- function(obj){
   meta <- obj@meta.data
-  meta <- meta %>% distinct(projid, .keep_all=TRUE)
+  meta <- meta %>% distinct(projid_region, .keep_all=TRUE)
   col_data <- data.frame(meta)
-  col_data <- col_data %>% remove_rownames %>% column_to_rownames(var="projid")
+  col_data <- col_data %>% remove_rownames %>% column_to_rownames(var="projid_region")
   return(col_data)
   write.csv(col_data, "coldata.csv")
 }
